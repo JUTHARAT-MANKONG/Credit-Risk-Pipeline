a81a67774ee86ebf17e7cd19495538f9f39ea2a2
Jutharat Mankong <jutharat.pjp@gmail.com>
2026-06-23 17:40:26 +0700

    delete file
    create bronze_transform.py

--- src/phase1_ingestion/separatte_source_data.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-
-def process_raw_bank_data(.data/raw/Financial_Ecosystem_Dataset_T1_5k_.csv) :
-    print(f"INFO : เริ่มประมวลผลไฟล์คัดแยกข้อมูลจาก CSV : {.data/raw/Financial_Ecosystem_Dataset_T1_5k_.csv}")
-    if not os.path.exists(file_path) :
-        raise FileNotFoundError(f"ไม่พบไฟล์ข้อมูลดิบที่ : {.data/raw/Financial_Ecosystem_Dataset_T1_5k_.csv}")
-
-
